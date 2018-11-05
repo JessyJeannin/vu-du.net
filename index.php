61d6c34821fc4f106c03883edacb572ab426c7b0
Jessy <jessy.jeannin@gmail.com>
2018-11-05 09:56:56 +0100

    Importation dossier

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vu-du.net</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="home.php"><img src="images/logo.png" alt="logo"></a>
+            <ul class="nav-link">
+                <li><a href="product.html">Notre produit</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="service.html">Nos services</a></li>
+                <li><a href="news.html">Actus</a></li>
+                <li><a href="contact.html">Prise de contact</a></li>
+                <li><a href="account.php">Mon compte</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="home-text">
+        <p>Ne restez pas invisible sur la toile. Offrez vous une présence web pour que votre activité soit reconnue de tous.
+    Soyez "Vu-Du.Net" à partir de 30,00€/mois</p>
+    </div>
+
+    <div class="section">
+
+        <h1 class="title1">Un domaine, un site</h1>
+        <p class="sous-texte">C’est suivant vos besoins que nous définissons les fonctionnalités à implémenter sur votre site internet.</p>
+
+            <section>
+            
+                <article class="product">
+                    <a href="">Responsive <br> Il est important aujourd’hui que votre site web soit lisible aussi bien sur un écran que sur un smartphone.</a>
+                </article>
+        
+                <article class="product">
+                    <a href="">Vitrine Web <br> Présentez de façon claire et précise ce que produit/propose votre entreprise à vos Net-visiteurs.</a>
+                </article>
+        
+                <article class="product">
+                    <a href="">E-Commerce <br> Optez pour une boutique en ligne afin de vous affranchir des heures de paperasse nécessaire à la facturation client.</a>
+                </article>
+                
+                <article class="product">
+                    <a href="">Modules spécifiques <br> Chaque activité vous est propre, c’est pourquoi les besoins sont différents (Prise de RDV, Publications d’évènements,etc…)</a>
+                </article>
+            </section>
+    </div>
+
+</body>
+
+</html>
